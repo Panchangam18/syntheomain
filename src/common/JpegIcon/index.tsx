80bc11710b7a4447507f7a61ae41ea7eec1e9b9b
Panchangam18 <156732789+madpanch@users.noreply.github.com>
2024-05-07 18:33:03 -0400

    Done?

--- src/common/JpegIcon/index.tsx
+++ src/common/JpegIcon/index.tsx
@@ -9,7 +9,7 @@ interface JpegIconProps {
 
 const JpegIcon = ({ src, width, height, alt }: JpegIconProps) => {
   const altText = alt || src;  // Provide a default alt text if none is provided
-  const imagePath = `/img/images/${src}`;  // Construct the image path dynamically
+  const imagePath = `${process.env.PUBLIC_URL}/img/images/${src}`;  // Construct the image path dynamically
 
   return (
     <img src={imagePath} alt={altText} width={width} height={height} />
